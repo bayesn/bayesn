37f6b6807a718d23f75bdc99e954bf818d997068
mattgrayling <mjpg1g18@soton.ac.uk>
2024-05-01 23:52:56 +0100

    Fixed bug stopping 4 chains running in parallel on CPU using ZLTN VI, removed unused imports

--- bayesn/zltn_utils.py
+++ bayesn/zltn_utils.py
@@ -12,6 +12,7 @@ from numpyro.distributions.util import is_prng_key, promote_shapes, validate_sam
 from jax import lax
 from jax.random import normal, exponential
 
+
 class _FirstPositive(_SingletonConstraint):
     event_dim = 1
 
@@ -19,15 +20,17 @@ class _FirstPositive(_SingletonConstraint):
         print("Constraint is being called")
         return (x[0] >= 0)
 
-
     def feasible_like(self, prototype):
         return jnp.zeros_like(prototype)
 
+
 firstpositive = _FirstPositive()
+
 
 @biject_to.register(firstpositive)
 def _transform_to_real(constraint):
     return IdentityTransform()
+
 
 class MultiZLTN(Distribution):
     arg_constraints = {
@@ -48,12 +51,12 @@ class MultiZLTN(Distribution):
         return firstpositive
 
     def __init__(
-        self,
-        loc=0.0,
-        covariance_matrix=None,
-        precision_matrix=None,
-        scale_tril=None,
-        validate_args=None,
+            self,
+            loc=0.0,
+            covariance_matrix=None,
+            precision_matrix=None,
+            scale_tril=None,
+            validate_args=None,
     ):
         if jnp.ndim(loc) == 0:
             (loc,) = promote_shapes(loc, shape=(1,))
@@ -90,7 +93,7 @@ class MultiZLTN(Distribution):
         self.sigma_21 = self.sigma[1:, :1]
         self.sigma_12 = self.sigma[:1, 1:]
 
-        self.zltn_dist = TruncatedNormal(self.mu_1, self.scale_tril[0][0], low = 0.)
+        self.zltn_dist = TruncatedNormal(self.mu_1, self.scale_tril[0][0], low=0.)
         # print("batch shape: ", batch_shape)
         super(MultiZLTN, self).__init__(
             batch_shape=batch_shape,
@@ -102,37 +105,36 @@ class MultiZLTN(Distribution):
         assert is_prng_key(key)
 
         first_samples = self.zltn_dist.sample(key, sample_shape)
-        sigma_11_inverse = 1./self.sigma_11
+        sigma_11_inverse = 1. / self.sigma_11
 
-        if sample_shape==():
+        if sample_shape == ():
             mu_hat = self.mu_2 + jnp.matmul(self.sigma_21, ((1.0 / self.sigma_11) * (first_samples - self.mu_1)).T)
             sigma_hat = self.sigma_22 - jnp.matmul((self.sigma_21 * sigma_11_inverse), self.sigma_12)
-            sigma_hat += 1.e-9 * jnp.eye(self.event_shape[0]-1)
+            sigma_hat += 1.e-9 * jnp.eye(self.event_shape[0] - 1)
 
             eps = normal(
-                key, shape=sample_shape + self.batch_shape + (self.event_shape[0]-1,)
+                key, shape=sample_shape + self.batch_shape + (self.event_shape[0] - 1,)
             )
             second_samples = mu_hat.T + jnp.squeeze(
                 jnp.matmul(jnp.linalg.cholesky(sigma_hat), eps[..., jnp.newaxis]), axis=-1
             )
-            return jnp.concatenate((first_samples,second_samples), axis=0)
+            return jnp.concatenate((first_samples, second_samples), axis=0)
 
-        mu_hat = self.mu_2[:,None] + jnp.matmul(self.sigma_21, ((1.0 / self.sigma_11) * (first_samples - self.mu_1)).T)
+        mu_hat = self.mu_2[:, None] + jnp.matmul(self.sigma_21, ((1.0 / self.sigma_11) * (first_samples - self.mu_1)).T)
         # print("mu hat", mu_hat)
         sigma_hat = self.sigma_22 - jnp.matmul((self.sigma_21 * sigma_11_inverse), self.sigma_12)
-        sigma_hat += 1.e-9 * jnp.eye(self.event_shape[0]-1)
+        sigma_hat += 1.e-9 * jnp.eye(self.event_shape[0] - 1)
 
         eps = normal(
-            key, shape=sample_shape + self.batch_shape + (self.event_shape[0]-1,)
+            key, shape=sample_shape + self.batch_shape + (self.event_shape[0] - 1,)
         )
         second_samples = mu_hat.T + jnp.squeeze(
             jnp.matmul(jnp.linalg.cholesky(sigma_hat), eps[..., jnp.newaxis]), axis=-1
         )
-        return jnp.concatenate((first_samples,second_samples), axis=1)
-
+        return jnp.concatenate((first_samples, second_samples), axis=1)
 
     def log_prob(self, value):
-        log_prob_x = self.zltn_dist.log_prob(value[...,0])
+        log_prob_x = self.zltn_dist.log_prob(value[..., 0])
         sigma_11_inverse = 1. / self.sigma_11
         # mu_hat = self.mu_2 + self.sigma_21*sigma_11_inverse*(value[0]- self.mu_1)
         # print(self.mu_1.shape)
@@ -140,12 +142,13 @@ class MultiZLTN(Distribution):
         # print(value[...,0].shape)
         # print(value[0][0].shape)
 
-        mu_hat = self.mu_2[...,None] + jnp.matmul(self.sigma_21, sigma_11_inverse * (value[...,0] - self.mu_1[None,...]))
+        mu_hat = self.mu_2[..., None] + jnp.matmul(self.sigma_21,
+                                                   sigma_11_inverse * (value[..., 0] - self.mu_1[None, ...]))
         sigma_hat = self.sigma_22 - jnp.matmul((self.sigma_21 * sigma_11_inverse), self.sigma_12)
         # print(mu_hat.shape, sigma_hat.shape, value[...,1:].shape)
         m = MultivariateNormal(mu_hat.T, sigma_hat)
 
-        log_prob_y_given_x = MultivariateNormal(mu_hat.T, sigma_hat).log_prob(value[...,1:])
+        log_prob_y_given_x = MultivariateNormal(mu_hat.T, sigma_hat).log_prob(value[..., 1:])
         # print(log_prob_x + log_prob_y_given_x)
         return log_prob_x + log_prob_y_given_x
 
@@ -160,19 +163,18 @@ class MultiZLTN(Distribution):
     #     print(log_prob_x + log_prob_y_given_x)
     #     return log_prob_x + log_prob_y_given_x
 
-class AutoMultiZLTNGuide(AutoContinuous):
 
+class AutoMultiZLTNGuide(AutoContinuous):
     # scale_tril_constraint = constraints.scaled_unit_lower_cholesky
     scale_tril_constraint = constraints.lower_cholesky
 
-
     def __init__(
-        self,
-        model,
-        *,
-        prefix="auto",
-        init_loc_fn=init_to_median,
-        init_scale=0.1,
+            self,
+            model,
+            *,
+            prefix="auto",
+            init_loc_fn=init_to_median,
+            init_scale=0.1,
     ):
         if init_scale <= 0:
             raise ValueError("Expected init_scale > 0. but got {}".format(init_scale))
@@ -191,12 +193,10 @@ class AutoMultiZLTNGuide(AutoContinuous):
     def get_base_dist(self):
         return dist.Normal(jnp.zeros(self.latent_dim), 1).to_event(1)
 
-
     def get_transform(self, params):
         loc = params["{}_loc".format(self.prefix)]
         scale_tril = params["{}_scale_tril".format(self.prefix)]
         return LowerCholeskyAffine(loc, scale_tril)
-
 
     def get_posterior(self, params):
         """
@@ -220,9 +220,8 @@ class My_Exponential(Distribution):
     def sample(self, key, sample_shape=()):
         assert is_prng_key(key)
         return (
-            exponential(key, shape=sample_shape + self.batch_shape) / self.rate
+                exponential(key, shape=sample_shape + self.batch_shape) / self.rate
         )
-
 
     @validate_sample
     def log_prob(self, value):
@@ -234,7 +233,7 @@ class My_Exponential(Distribution):
 
     @property
     def variance(self):
-        return jnp.reciprocal(self.rate**2)
+        return jnp.reciprocal(self.rate ** 2)
 
     def cdf(self, value):
         return -jnp.expm1(-self.rate * value)
