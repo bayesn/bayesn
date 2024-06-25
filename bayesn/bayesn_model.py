dd7e761dc59dccb63342d24d1810b136f9f8e156
mattgrayling <mjpg1g18@soton.ac.uk>
2024-06-25 12:17:06 +0100

    Testing different density matrices

--- bayesn/bayesn_model.py
+++ bayesn/bayesn_model.py
@@ -2100,7 +2100,7 @@ class SEDmodel(object):
         elif args['mode'].lower() == 'training_poprv':
             nuts_kernel = NUTS(self.train_model_popRV, adapt_step_size=True, target_accept_prob=0.8,
                                init_strategy=init_strategy,
-                               dense_mass=False, find_heuristic_step_size=False, regularize_mass_matrix=False,
+                               dense_mass=True, find_heuristic_step_size=False, regularize_mass_matrix=False,
                                step_size=0.1)
         elif args['mode'].lower() == 'training_poprv_noeps':
             nuts_kernel = NUTS(self.train_model_popRV_noeps, adapt_step_size=True, target_accept_prob=0.8,
