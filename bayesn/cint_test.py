bb8a804095fad82ec7bea036fca20f8e8d8187e0
mattgrayling <mjpg1g18@soton.ac.uk>
2025-10-03 18:04:46 +0100

    Testing on simulations

--- bayesn/cint_test.py
+++ bayesn/cint_test.py
@@ -10,14 +10,13 @@ bands = ['g_PS1', 'r_PS1']
 cint = np.linspace(-0.1, 0.1, N)
 theta = np.linspace(-0.1, 0.1, N)
 
-model_path = '/Users/matt/Documents/bayesn-input/cint_train/T21_sim_train_theta0AV0W10'
+model_path = '/Users/matt/Documents/bayesn-input/cint_train/T21_sim_train_theta0AV0'
 model_chains = os.path.join(model_path, 'chains.pkl')
 
 model = SEDmodel(load_model=os.path.join(model_path, 'bayesn.yaml'))
 T21_model = SEDmodel(load_model='T21_model')
 
 lc = model.simulate_light_curve(t, 1, bands, theta=0, AV=0, mu=0, del_M=0, eps=0, mag=True)[0]
-print(lc.shape)
 m150 = lc[0, 0] - lc[1, 0]
 
 # t2 = np.arange(-10, 40, 1)
@@ -42,6 +41,61 @@ m150 = lc[0, 0] - lc[1, 0]
 # plt.gca().invert_yaxis()
 # plt.show()
 # print(lc.shape)
+# pkill
+
+T21_N = 10
+T21_thetas = np.linspace(-2, 2, T21_N)
+
+t2 = np.arange(-10, 40, 1)
+lc = T21_model.simulate_light_curve(t2, T21_N, bands, theta=T21_thetas, AV=0, mu=0, del_M=0, eps=0, mag=True)[0]
+for i in range(T21_N):
+    plt.plot(t2, lc[:len(t2), i], label=f'{T21_thetas[i]:.2f}')
+# m15 = lc[0, :] - lc[1, :]
+# plt.plot(theta, m15)
+plt.legend()
+plt.gca().invert_yaxis()
+plt.show()
+
+lc = T21_model.simulate_light_curve(np.array([0, 15, 30]), T21_N, ['g_PS1', 'r_PS1'], theta=T21_thetas, AV=0, mu=0, del_M=0, eps=0, mag=True)[0]
+print(lc.shape)
+m, m15, c = lc[0, :], lc[1, :] - lc[0, :], lc[0, :] - lc[3, :]
+# print(m.shape, m15.shape, c.shape)
+# plt.scatter(c, m)
+# plt.gca().invert_yaxis()
+# plt.show()
+plt.scatter(c, m15)
+plt.show()
+plt.scatter(T21_thetas, m15)
+plt.show()
+plt.scatter(T21_thetas, c)
+plt.show()
+
+lc = T21_model.simulate_light_curve(np.array([0, 10, 30]), 500, ['g_PS1', 'r_PS1'], theta=0, AV=0, mu=0, del_M=0, eps=None, mag=True)[0]
+print(lc.shape)
+m, m15, c = lc[0, :], lc[1, :] - lc[0, :], lc[0, :] - lc[3, :]
+# print(m.shape, m15.shape, c.shape)
+# plt.scatter(c, m)
+# plt.gca().invert_yaxis()
+# plt.show()
+print('-->', np.std(c))
+plt.scatter(c, m15)
+plt.xlabel(r'c$_{int}$')
+plt.ylabel(r'$\Delta m_{10}$')
+plt.show()
+
+t2 = np.arange(-10, 40, 1)
+lc = T21_model.simulate_light_curve(t2, 20, bands, theta=0, AV=0, mu=0, del_M=0, eps=None, mag=True)[0]
+for i in range(20):
+    plt.plot(t2, lc[:len(t2), i])
+# m15 = lc[0, :] - lc[1, :]
+# plt.plot(theta, m15)
+plt.gca().invert_yaxis()
+plt.show()
+
+# plt.scatter(T21_thetas, m15)
+# plt.show()
+# plt.scatter(T21_thetas, c)
+# plt.show()
 # pkill
 
 lc = model.simulate_light_curve_cint(np.array([0]), 1, bands, model_chains, theta=0, AV=0, mu=0, del_M=0, cint=0, eps=0, mag=True)[0]
